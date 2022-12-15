01dd5ff70a51a54a4d0955acbd5ff62ecbf9d143
Jurand76 <jurand@interia.pl>
2022-12-15 22:56:50 +0100

    feat: added code

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        float result;
+        result = toBeDivided / 5;
+        System.out.println(result);
     }
 }
